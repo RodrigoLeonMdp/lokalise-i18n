f3bb2a0eded5cd5be6c1b3cc41af296b1a001a45
Rodrigo Leon <rodrigoleonmdp@gmail.com>
2025-04-02 08:21:28 -0300

    Init files

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <section>
+      <h1>About page</h1>
+      <p>Personal information about the person...</p>
+    </section>
+  );
+};
+
+export default AboutPage;
